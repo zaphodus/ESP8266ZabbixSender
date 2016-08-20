8de432df90b5493d6854bf4f0c45d1397fca186b
erscl <email@erscl.org>
2016-08-20 23:46:32 +0900

    Fix code format of sample code

--- sample_ESP8266ZabbixSender/sample_ESP8266ZabbixSender.ino
+++ sample_ESP8266ZabbixSender/sample_ESP8266ZabbixSender.ino
@@ -7,9 +7,9 @@ String ssid = "AP_SSID";
 String pass = "AP_PASSWD";
 
 /* Zabbix server setting */
-#define SERVERADDR 192, 168, 35, 14  // Zabbix server Address
-#define ZABBIXPORT 10051			 // Zabbix erver Port
-#define ZABBIXAGHOST "IOTBOARD_00"   // Zabbix item's host name
+#define SERVERADDR 192, 168, 35, 14 // Zabbix server Address
+#define ZABBIXPORT 10051			// Zabbix erver Port
+#define ZABBIXAGHOST "IOTBOARD_00"  // Zabbix item's host name
 
 boolean checkConnection();
 
@@ -21,24 +21,24 @@ void setup() {
 	WiFi.begin(ssid.c_str(), pass.c_str());
 	while (!checkConnection()) {
 	}
-	zSender.Init(IPAddress(SERVERADDR), ZABBIXPORT, ZABBIXAGHOST);  // Init zabbix server information
+	zSender.Init(IPAddress(SERVERADDR), ZABBIXPORT, ZABBIXAGHOST); // Init zabbix server information
 }
 
 void loop() {
 	static int counter1 = 0;
 	static int counter2 = 0;
-	checkConnection();							   // Check wifi connection
-	zSender.ClearItem();						   // Clear ZabbixSender's item list
-	zSender.AddItem("counter1", (float)counter1);  // Exmaple value of zabbix trapper item
-	zSender.AddItem("counter2", (float)counter2);  // Exmaple value of zabbix trapper item
-	if (zSender.Send() == EXIT_SUCCESS) {		   // Send zabbix items
+	checkConnection();							  // Check wifi connection
+	zSender.ClearItem();						  // Clear ZabbixSender's item list
+	zSender.AddItem("counter1", (float)counter1); // Exmaple value of zabbix trapper item
+	zSender.AddItem("counter2", (float)counter2); // Exmaple value of zabbix trapper item
+	if (zSender.Send() == EXIT_SUCCESS) {		  // Send zabbix items
 		Serial.println("ZABBIX SEND: OK");
 	} else {
 		Serial.println("ZABBIX SEND: NG");
 	}
 	counter1 += 1;
 	counter1 *= 2;
-	delay(1000);  // wait 1sec
+	delay(1000); // wait 1sec
 }
 
 boolean checkConnection() {
